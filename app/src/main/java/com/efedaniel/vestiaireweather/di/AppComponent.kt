1a94388dc909fcd6e116af95b71bc8535c2b7545
EfeDaniel <EfeEjemudaro@gmail.com>
2021-10-27 23:48:20 +0100

    setup viewmodels

--- app/src/main/java/com/efedaniel/vestiaireweather/di/AppComponent.kt
+++ app/src/main/java/com/efedaniel/vestiaireweather/di/AppComponent.kt
@@ -2,6 +2,8 @@ package com.efedaniel.vestiaireweather.di
 
 import android.app.Application
 import com.efedaniel.vestiaireweather.VestiaireWeatherApp
+import com.efedaniel.vestiaireweather.presentation.weather.details.WeatherDetailsFragment
+import com.efedaniel.vestiaireweather.presentation.weather.list.WeatherListFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -10,7 +12,8 @@ import javax.inject.Singleton
 @Component(modules = [APIServiceModule::class, ViewModelModule::class])
 interface AppComponent {
 
-    fun inject(target: VestiaireWeatherApp)
+    fun inject(target: WeatherListFragment)
+    fun inject(target: WeatherDetailsFragment)
 
     @Component.Builder
     interface Builder {
