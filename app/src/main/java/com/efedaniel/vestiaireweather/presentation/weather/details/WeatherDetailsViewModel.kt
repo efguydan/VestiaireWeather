3dd376f7861a4fb53219ce547f994cb32629a68e
EfeDaniel <EfeEjemudaro@gmail.com>
2021-10-27 23:48:20 +0100

    setup viewmodels

---
+++ app/src/main/java/com/efedaniel/vestiaireweather/presentation/weather/details/WeatherDetailsViewModel.kt
@@ -0,0 +1,12 @@
+package com.efedaniel.vestiaireweather.presentation.weather.details
+
+import com.efedaniel.vestiaireweather.presentation.base.BaseViewModel
+import javax.inject.Inject
+
+class WeatherDetailsViewModel @Inject constructor(
+
+): BaseViewModel() {
+
+
+
+}
---
+++ app/src/main/java/com/efedaniel/vestiaireweather/presentation/weather/list/WeatherListViewModel.kt
@@ -0,0 +1,12 @@
+package com.efedaniel.vestiaireweather.presentation.weather.list
+
+import com.efedaniel.vestiaireweather.presentation.base.BaseViewModel
+import javax.inject.Inject
+
+class WeatherListViewModel @Inject constructor(
+
+): BaseViewModel() {
+
+
+
+}
